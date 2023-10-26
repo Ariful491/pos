cde2bdd260591dc0d36b46a32b6a9b13d7ff5b4d
Ariful Hoque <arifulhoque091@gmail.com>
2023-10-26 23:10:45 +0600

    add column in permissions table

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 
 class PermissionSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+      
     }
 }
