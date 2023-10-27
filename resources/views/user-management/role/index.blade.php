e0e2e48fe5f784345d07a62ff4f2ee0f1c444e0d
Ariful Hoque <arifulhoque091@gmail.com>
2023-10-27 11:44:49 +0600

    role index blade file.

--- resources/views/user-management/role/index.blade.php
+++ resources/views/user-management/role/index.blade.php
@@ -7,9 +7,7 @@
     <div class="container mt-2">
        <div class="row">
           <div class="max-w-7xl  justify-center  sm:px-6 lg:px-8">
-              <button type="button"
-                      class="inline-flex mb-2 items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
-
+              <a href="" class="inline-flex mb-2 items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                   <div>
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                            stroke="currentColor" class="w-6 h-6 mr-2 ">
@@ -19,7 +17,7 @@
                   <div>
                       Add Role
                   </div>
-              </button>
+              </a>
           </div>
        </div>
     </div>
