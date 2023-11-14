878654a97b2af6c9f9f1efa07e4fa4d61c15f485
Ariful491 <arifulhoque091@gmail.com>
2023-11-14 20:04:00 +0600

    Store Role with permissions .

--- resources/views/user-management/role/index.blade.php
+++ resources/views/user-management/role/index.blade.php
@@ -64,12 +64,14 @@
                             </thead>
                             <tbody>
                             @foreach($roles as $index=>$role)
+
                                 <tr>
                                     <td>{{$index+1}}</td>
                                     <td>{{$role->name}}</td>
                                     <td>{{$role->name}}</td>
                                     <td>{{$role->name}}</td>
                                 </tr>
+
                             @endforeach
                             </tbody>
                         </table>
